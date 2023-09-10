cd2a22cb46db782549f0ad75838413cb5fb5d927
Jason Walker <walkerjason@live.com>
2023-09-10 14:09:40 -0400

    Added help to Add-DefaultProperty

--- Private/Add-DefaultProperty.ps1
+++ Private/Add-DefaultProperty.ps1
@@ -1,3 +1,13 @@
+<#
+    .SYNOPSIS
+        Adds WifiAdapterName and InterfaceGuid to all the objects that are returned.
+
+    .PARAMETER $InputObject
+        The object the two properties will be added to.
+
+    .PARAMETER InterfaceInfo
+        The interfaceInfo object that the WiFiAdapterName and InterfaceGuid come from.
+#>
 function Add-DefaultProperty
 {
     [CmdletBinding()]
@@ -12,7 +22,7 @@ function Add-DefaultProperty
         $InterfaceInfo
     )
 
-    Add-Member -InputObject $InputObject -MemberType 'NoteProperty' -Name 'WiFiAdapter' -Value $InterfaceInfo.Name -Force
+    Add-Member -InputObject $InputObject -MemberType 'NoteProperty' -Name 'WiFiAdapterName' -Value $InterfaceInfo.Name -Force
     Add-Member -InputObject $InputObject -MemberType 'NoteProperty' -Name 'InterfaceGuid' -Value $InterfaceInfo.InterfaceGuid -Force
 
     return $InputObject
